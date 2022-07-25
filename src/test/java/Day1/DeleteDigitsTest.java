f045f545621a6d13706a04069ded6af174a20a02
Slava Dobrov <dobrov.slava@yandex.ru>
2022-07-25 05:59:13 +0300

    Added two more tests for method clearString from class DeleteDigits. Tests passed successfully.

--- src/test/java/Day1/DeleteDigitsTest.java
+++ src/test/java/Day1/DeleteDigitsTest.java
@@ -8,10 +8,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class DeleteDigitsTest {
 
     @Test
-    void clearString() {
+    void clearStringTest1() {
+
         DeleteDigits line = new DeleteDigits();
         String result = line.ClearString("8736428129ijh");
         String expected = "ijh";
-        Assertions.assertEquals(expected, result);
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    void clearStringTest2() {
+
+        DeleteDigits line = new DeleteDigits();
+        String result = line.ClearString("iusd8734rvf8326");
+        String expected = "iusdrvf";
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    void clearStringTest3() {
+
+        DeleteDigits line = new DeleteDigits();
+        String result = line.ClearString("87c6v39jn30");
+        String expected = "cvjn";
+        assertEquals(expected, result);
+
     }
 }
