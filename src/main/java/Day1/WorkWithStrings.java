5068e27bc8a76a0f94e3dc0ad7f00f25900a4244
Slava Dobrov <dobrov.slava@yandex.ru>
2022-07-25 04:51:28 +0300

    Added method which delete digits from string. I used construction try catch.

--- src/main/java/Day1/WorkWithStrings.java
+++ src/main/java/Day1/WorkWithStrings.java
@@ -1,4 +1,31 @@
 package Day1;
 
 public class WorkWithStrings {
+
+    public static void main(String[] args) {
+
+        System.out.println(ClearString("901754787634321301936111uh1"));
+
+    }
+
+    public static String ClearString(String str) {
+
+        String result = "";
+        String[] strArray = str.split("");
+
+        for (int i = 0; i < strArray.length; i++) {
+
+            try{
+
+                int j = Integer.parseInt(strArray[i]);
+
+            } catch (NumberFormatException e) {
+
+                result = result + strArray[i];
+
+            }
+        }
+
+        return result;
+    }
 }
